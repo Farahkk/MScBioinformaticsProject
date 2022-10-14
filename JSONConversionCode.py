89d8d1b82ea6117d60f4ca2a1038c09d489c7611
Farahkk <kf002@mail.cryst.bbk.ac.uk>
2022-10-14 11:07:13 +0100

    Version with error

--- JSONConversionCode.py
+++ JSONConversionCode.py
@@ -4,14 +4,57 @@
  
 import json
 
-filename = 'Downloads/Bioinformatics/MSc_Project/sample_files/12054.txt'
+filename = '../data/12054.txt'
 dic = {}
+
+# This is our state machine to indicate if we are in one of the sequence
+# blocks - to start we aren't
+
+inSequence = False
+
+#while(readFromFile(line))
 
 with open(filename, 'r') as f:
     #read lines from file and remove spaces to get valid data only
         for line in f:
-            if ',' not in line:
-                continue
-            key, value = line.strip(';,\n').split(':', 1)
-            dic[key] = value 
+            #if ',' not in line:
+                #continue
+            #key, value = line.strip(';,\n').split(':', 1)
+            #dic[key] = value 
+        #print(json.dumps(dic, indent=2))
+        
+#while(readFromFile(line))          
+            if line == '//': #This indicates the end of a sequence block
+
+                inSequence = False
+
+            elif line == "Chain" or line == "Heavy Chain" or line == "Light Chain": # Start of a sequence block 
+
+            #remove-colon-from-end-of-line;
+                 key = line;
+                 data[key] = '';     # Initialize data storage 
+        
+                 inSequence = True;
+        
+            elif inSequence:    # We are within a sequence block
+    
+           #cleanup(line):      # Strip whitespace and return character
+                data[key] += line;  # Append sequence information to the data
+
+            else:                # Normal line
+   
+                (key, value) = line.strip(';,\n').split(':', 1);
+
+           # Special case of note records where we add '-' and the previous line's key
+            if key == 'Note[:]':
+    
+                key += "-" + previousKey;
+      
+                data[key] = value;
+   
+                previousKey = key;     # Update the previous key to be the key from this line
+            
+            #continue
+        key, value = line.strip('; ,\n').split(':', 1)
+        dic[key] = value 
         print(json.dumps(dic, indent=2))
--- JSONConversionCode0.py
+++ JSONConversionCode0.py
@@ -1,46 +1,59 @@
 #!/usr/bin/python3
 
-import re
-
+# Python program to convert text file to JSON
+ 
 import json
 
-import pprint
-
 filename = '../data/12054.txt'
+dic = {}
 
 with open(filename, 'r') as f:
-    for lines in f:
-        file = f.readlines()
+    #read lines from file and remove spaces to get valid data only
+        for line in f:
+            if ',' not in line:
+                continue
+            #key, value = line.strip(';,\n').split(':', 1)
+            #dic[key] = value 
+        #print(json.dumps(dic, indent=2))
         
-        dictionary = dict(re.findall(r'(.{1,}):(.{1,})', str(file))) 
-        #. Match any character except newline (normally)
-        #{n,} Match at least n times
-        
-        #json = json.dumps(dict(re.findall(r'(.{1,}):(.{1,})', str(file))))
-        
-        #str() to convert the file to a string
-        #json.dumps function to group similar keys together
-        #pprint.pprint function to print key: value pairs on new line
-        #pprint.PrettyPrinter function to print each entry to a new line as dictionary     width is too short when printed
+        # This is our state machine to indicate if we are in one of the sequence
+# blocks - to start we aren't
+        inSequence = false;
 
-    print(dictionary)
-    #pprint.pprint(dictionary)
-    #print(json)
-    #pprint.pprint(json)
-    #better = pprint.PrettyPrinter(width=200)#adjust width argument accordingly
-    #better.pprint(dictionary)
-    #better.pprint(json)
-import re
-
-filename = '../data/12054.txt'
-
-with open(filename, 'r') as f:
-    for lines in f:
-        file = f.readlines()
+        while(readFromFile(line))
         
-        dictionary = dict(re.findall(r'(.{1,}):(.{1,})', str(file))) 
-        #. Match any character except newline (normally)
-        #{n,} Match at least n times
+       if(line == "//")  # This indicates the end of a sequence block
+
+      inSequence = false;
+
+       else if((line starts with "Chain")   ||  # Start of a sequence block
+           (line starts with "Heavy Chain") ||
+           (line starts with "Light Chain"))
+       
+       remove-colon-from-end-of-line;
+       key = line;
+       data[key] = '';     # Initialize data storage 
+       inSequence = true;
         
-    print(dictionary)
+       else if(inSequence)    # We are within a sequence block
     
+       cleanup(line);      # Strip whitespace and return character
+       data[key] += line;  # Append sequence information to the data
+
+       else                   # Normal line
+   
+      (key, value) = split-on-colon(line);
+
+      # Special case of note records where we add '-' and the previous line's key
+       if(key starts with Note)
+    
+       key += "-" + previousKey;
+      
+       data[key] = value;
+   
+       previousKey = key;     # Update the previous key to be the key from this line
+
+
+       key, value = line.strip(';,\n').split(':', 1)
+            dic[key] = value 
+       print(json.dumps(dic, indent=2))
